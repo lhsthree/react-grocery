9e903489027505b11e3fd2e34274657e831ef52b
lhsthree <lhsthree@gmail.com>
2021-02-13 12:17:32 -0600

    feat: add functionality to clear list button

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     } else if(name && isEditing){
       //deal with edit
     } else {
-      //show alert
+      showAlert(true, 'success', 'item added to the list')
       const newItem ={
         id: new Date().getTime().toString(),
         title: name
@@ -33,6 +33,12 @@ function App() {
   const showAlert = (show=false, type="", msg="") => {
     setAlert({show,type,msg})
   }
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'list cleared')
+    setList([])
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -54,7 +60,7 @@ function App() {
       {list.length > 0 &&
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>clear items</button>
         </div>
       }
     </section>
